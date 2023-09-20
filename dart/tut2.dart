e3e72c8c7eb9a9010c6e4bb6a39b0f566e91bdf3
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-20 19:35:07 +0530

    do it

--- dart/tut2.dart
+++ dart/tut2.dart
@@ -1,6 +1,17 @@
-main(){
-  //this code is to print name of student
-  /*multiline comment
-  syntex*/
-  print("Soumyajit");
+// main(){
+//   //this code is to print name of student
+//   /*multiline comment
+//   syntex*/
+//   print("Soumyajit\nBabai");
+//   //pyramid using \n
+//   print("   *   \n  ***  \n ***** \n*******");
+// }
+//variable in dart
+void main(){
+  //how to declear variable
+  int num;
+  num=34;
+  num++;
+  num=-19;
+  print(num);
 }
