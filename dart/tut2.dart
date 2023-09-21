e37b92026730ec05db1fb68a23933b605c6b2e8a
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-22 01:12:39 +0530

    do kit

--- dart/tut2.dart
+++ dart/tut2.dart
@@ -1,19 +1,23 @@
-// main(){
-//   //this code is to print name of student
-//   /*multiline comment
-//   syntex*/
-//   print("Soumyajit\nBabai");
-//   //pyramid using \n
-//   print("   *   \n  ***  \n ***** \n*******");
+// // main(){
+// //   //this code is to print name of student
+// //   /*multiline comment
+// //   syntex*/
+// //   print("Soumyajit\nBabai");
+// //   //pyramid using \n
+// //   print("   *   \n  ***  \n ***** \n*******");
+// // }
+// //variable in dart
+// void main(){
+//   //how to declear variable
+// //   int num1,num2,num3,num4;
+// //   num1=90;
+// //   num2=87;
+// //   num3=27;
+// //   num4=3;
+// //  int  result=(num1+num2)-(num3*num4);
+// int num1,num2;
+// double divide;
+// num1=27;
+// num2=9;
+// divide=num1/num2;
 // }
-//variable in dart
-void main(){
-  //how to declear variable
-  int num1,num2,num3,num4;
-  num1=90;
-  num2=87;
-  num3=27;
-  num4=3;
- int  result=(num1+num2)-(num3*num4);
- print(result);
-}
---
+++ variable.dart
@@ -0,0 +1,9 @@
+main(){
+  int num1,num2;
+  double divide;
+  num1=56;
+  num2=4;
+  divide=(num1/num2);
+  print(divide);
+
+}
