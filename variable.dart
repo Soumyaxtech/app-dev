4479fc544d9e9666db0ceb3f399a484f4a60ab33
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-23 09:20:07 +0530

    do it

--- variable.dart
+++ variable.dart
@@ -1,9 +1,15 @@
-main(){
-  int num1,num2;
-  double divide;
-  num1=56;
-  num2=4;
-  divide=(num1/num2);
-  print(divide);
+import 'dart:io';
+void main() {
+  var input = stdin;
 
+  // Prompt for name input
+  print('Enter your name:');
+  String? name = input.readLineSync();
+
+  // Prompt for age input
+  print('Enter your age:');
+  int ? age = int.parse(input.readLineSync()!);
+
+  // Display a greeting along with the age
+  print('Hello, $name! You are $age years old.');
 }
