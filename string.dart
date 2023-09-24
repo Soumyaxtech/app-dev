c229df722e074623edd24dd0b6580b8f51333b92
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-24 09:49:29 +0530

    do it

--- string.dart
+++ string.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
 void main(){
-  String name='HELLOWORLD\n';
-  String age='0123456789';
+  String name='HELLOWORLD';
+  String age='\n0123456789';
   print(name.length);
   print(name[4]);//this is how we get any value of the string
   String result;
   result= name+ age;//concatination of string
   print(result);
-  print(name.toLowerCase());
-  print(name.toUpperCase());
+  print(name.toLowerCase());//convert a string into lower case
+  stdout.write(name.toUpperCase());//convert a string into upper case
+  print(name.split(''));//for splitting a string 
+  print(name.replaceAll(name, 'abcdef'));// to replace a string
+  print(name.contains('O'));/*for checking if the string contain that thing.if it contain
+  then it returns true if it not then return false*/
 }
