363e2b1368599c01ae6a48440da58346aa1f87cb
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-10-09 23:05:41 +0530

    do it

--- pracode.dart
+++ pracode.dart
@@ -1,19 +1,19 @@
 // //add ,subtract,multiple of two numbers
-// import 'dart:io';
-// void main(){
-//   var input=stdin;
-//   stdout.write("enter number 1 ");
-//   int ? num1=int.parse(input.readLineSync()!);
-//   stdout.write("enter number 2 ");
-//   int ? num2=int.parse(input.readLineSync()!);
-//   int sum=num1+num2;
-//   int sub=num2-num1;
-//   int mul=num1*num2;
-//   double div=num2/num1;
-//   stdout.write("sum is $sum\n");
-//   stdout.write("sub is $sub\n");
-//   stdout.write("mul is $mul\n");
-//   stdout.write("div is $div");
+import 'dart:io';
+void main(){
+  var input=stdin;
+  stdout.write("enter number 1 ");
+  int  num1=int.parse(input.readLineSync()!);
+  stdout.write("enter number 2 ");
+  int  num2=int.parse(input.readLineSync()!);
+  int sum=num1+num2;
+  int sub=num2-num1;
+  int mul=num1*num2;
+  double div=num2/num1;
+  stdout.write("sum is $sum\n");
+  stdout.write("sub is $sub\n");
+  stdout.write("mul is $mul\n");
+  stdout.write("div is $div");
 // }
 
 
@@ -63,4 +63,4 @@
 //   int ? cel=int.parse(input.readLineSync()!);
 //   double fer=(((9/5)*cel)+32);
 //   stdout.write("the temp in fer is $fer");
-// }
+}
