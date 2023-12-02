a4a308eb670804c2c9e7e22b2110fb93a83a2d69
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-12-02 09:27:09 +0530

    bool userinput

--- bool.dart
+++ bool.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
 void main(){
-  int num1=12,num2=4;
+  var input=stdin;
+   stdout.write("enter num1 ");
+  int ? num1 = int.parse(input.readLineSync()!);
+   stdout.write("enter num2 ");
+  int ? num2 = int.parse(input.readLineSync()!);
   bool result=true;
   bool iseven=true;
   result=num1>num2;
