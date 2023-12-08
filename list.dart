7c49eeab00551d5aae0ab6f72b677bf66b0bd527
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-12-08 09:37:38 +0530

    do it

--- list.dart
+++ list.dart
@@ -1,9 +1,16 @@
 void main(){
   List <int>age=[2,6,8,9];//syntax for list
   List<String>name=['soumya','rohit','ranajit','soumyadeep'];
+  dynamic data =[23,'soumyajit'];//  in dynamic we can store diff datatype together
   print(name.length );
   print(age.length);  //to find length of list
+  print(data.length);
   for(int i=0;i<age.length;i++){
    print("my name is ${name[i]} and age is ${age[i]}");
+  }
+  int i=0;
+  while( i<data.length){
+    print(data[i]);
+    i++;
   }
 }
