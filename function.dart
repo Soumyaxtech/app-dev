761b8715cd5c35b031ed07aec40c094597fd52b5
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-12-13 00:31:43 +0530

    parameter passing

--- function.dart
+++ function.dart
@@ -1,8 +1,27 @@
+// import "dart:io";
+// void main(){
+//   sum();
+// }
+// void sum(){
+//  stdout.write("enter a number ");
+//  int x = int . parse(stdin.readLineSync()!);
+//  if(x%2==0){
+//   stdout.write("$x is an even number ");
+//  } 
+//  else{
+//   stdout.write("$x is an odd number ");
+//  }
+// }
 import "dart:io";
 void main(){
-  sum(430,300);
+   stdout.write("enter number 1 ");
+ int num1 = int . parse(stdin.readLineSync()!);
+  stdout.write("enter number 2 ");
+ int num2 = int . parse(stdin.readLineSync()!);
+
+  sum(num1,num2);
 }
-void sum(int num1,int num2){
-  
-  stdout.write(num1+num2);
+void sum(int num1,num2){
+
+ stdout.write("the number is ${num1+num2}");
 }
