60c2fe7a364668eeddcc7ae13dc984129129e60b
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-12-17 20:30:27 +0530

    future function

--- async.dart
+++ async.dart
@@ -1,18 +1,29 @@
 // import "dart:io";
 // void main()async{
 //   stdout.write("Soumyajit");
-//   await Future.delayed(Duration(seconds: 3));
+//   await Future.delayed(Duration(seconds: 3));   // to delay some time in run code
 //   stdout.write(" koley");
 // }
 import "dart:io";
 void main(){
-  print(num());
-  num().then((value){
+  num().then((value){   // we have to write "then" to print future function
+  print(value);
+}
+).catchError((onError){
+  print(onError);
+});
+set().then((value){   // we have to write "then" to print future function
   print(value);
 }
 );
+
 }
-Future<int>num()async{
- stdout.write("soumyajit");
-  return 120;
+Future<bool>num()async{    //  to call future function
+ stdout.write("Soumyajit\n");
+  return true;
+}
+Future<int> set()async{
+  stdout.write("Koley\n");
+  await Future.delayed(Duration(seconds: 10)); 
+  return 90;
 }
